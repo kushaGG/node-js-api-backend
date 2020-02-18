3b03062c3c96be4fe7ad9ef1bf0abff566399cd2
kushaGG <artem.kushniruk29@gmail.com>
2020-02-18 15:23:06 +0200

    fix output user details from verifyToken

--- services/verifyToken.js
+++ services/verifyToken.js
@@ -1,13 +1,14 @@
 const jwt = require('jsonwebtoken');
 const User = require('../models/User')
 
-module.exports = function(req, res, next) {
+module.exports = async function(req, res, next) {
     const token = req.header('auth-token');
     if (!token) return res.status(401).json('Access denied')
 
     try {
         const verified = jwt.verify(token, process.env.TOKEN_SECRET)
-        req.user = verified;
+        const findUser = await User.findOne({_id: verified._id})
+        req.user = findUser;
         next();
     }
     catch (err) {
